bd40865c434341de807ddd2f1178583e20a3dfa8
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-10-25 22:34:29 -0500

    gl context init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+const c = document.getElementsByTagName("canvas")![0];
+const gl = c.getContext("webgl");
+
+if (gl === null) {
+	alert("Unable to initialize WebGL.");
+	window.close();
+}
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
