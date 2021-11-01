870018a46d661db6ed70c3586b507df376b2c4ea
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-11-01 18:04:06 -0500

    simplify

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -20,38 +20,27 @@ Promise.all([
 		new ImageData(f.patch, f.dims.width, f.dims.height)
 	]);
 
-	// create base matrix
-	const base = new Float32Array(9);
-	base[0] = 1 / width;
-	base[4] = 1 / height;
-	base[8] = 1;
-
-	console.log(base);
+	const mat = new Float32Array(9);
 
 	const l = frames.length;
 	let i = 0;
 	function renderFrame() {
 		const [frame, img] = frames[i];
 
-		// translate base matrix
-		const mat = base.slice(0);
+		// calculate
+
 		const { width: sx, height: sy, left: tx, top: ty } = frame.dims;
 
-		mat[6] = mat[0] * (sx + tx * 2) - 1;
-		mat[7] = 1 - mat[4] * (sy + ty * 2);
+		mat[0] = sx / width;
+		mat[4] = sy / height;
 
-		mat[0] *= sx;
-		mat[1] *= sx;
-		mat[2] *= sx;
-
-		mat[3] *= sy;
-		mat[4] *= sy;
-		mat[5] *= sy;
+		mat[6] = (sx + tx * 2) / width - 1;
+		mat[7] = 1 - (sy + ty * 2) / height;
 
 		sess.render(sess.texture(img), mat);
 		i = ++i % l;
 
-		setTimeout(renderFrame, frame.delay);
+		requestAnimationFrame(renderFrame);
 	}
 
 	requestAnimationFrame(renderFrame);
